74b015330ef57305dd3306c1aba5221d049296bf
edard3v <edard3v@gmail.com>
2024-12-25 00:35:11 -0500

    iniciando template

---
+++ src/components/shared/Footer/Network/networks.ts
@@ -0,0 +1,39 @@
+import Discord from "./icons/Discord.astro";
+import Github from "./icons/Github.astro";
+import Instagram from "./icons/Instagram.astro";
+import Linkedin from "./icons/Linkedin.astro";
+import Youtube from "./icons/Youtube.astro";
+import X from "./icons/X.astro";
+
+export const NETWORKS = [
+  {
+    display: "github",
+    url: "https://github.com/edard3v",
+    Icon: Github,
+  },
+  {
+    display: "youtube",
+    url: "https://www.youtube.com/@edard3v",
+    Icon: Youtube,
+  },
+  {
+    display: "linkedin",
+    url: "https://www.linkedin.com/in/edard3v",
+    Icon: Linkedin,
+  },
+  {
+    display: "discord",
+    url: "https://discord.gg/HUfnRPjBHm",
+    Icon: Discord,
+  },
+  {
+    display: "instagram",
+    url: "https://www.instagram.com/edard3v/",
+    Icon: Instagram,
+  },
+  {
+    display: "X",
+    url: "https://x.com/edard3v",
+    Icon: X,
+  },
+];
---
+++ src/pages/router.ts
@@ -0,0 +1,11 @@
+export const ROUTER = {
+  home: {
+    href: "/home/",
+    display: "Inicio",
+  },
+
+  contact: {
+    href: "/contact/",
+    display: "Cotacto",
+  },
+};
