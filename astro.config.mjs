6258eb724d560d58eb045f02a0f4088aa47f22a5
edard3v <edard3v@gmail.com>
2025-03-10 10:03:16 -0500

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  // vite: {
-  //   build: {
-  //     assetsInlineLimit: 0,
-  //   },
-  // },
+  vite: {
+    build: {
+      assetsInlineLimit: 0,
+    },
+  },
+
+  integrations: [icon()],
 });
