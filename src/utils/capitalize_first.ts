b179e1832e8f3e4b634907c59448ad6b49a3c539
edard3v <edard3v@gmail.com>
2025-03-11 21:33:12 -0500

    refactor

--- src/utils/capitalize_first.ts
+++ src/utils/capitalize_first.ts
@@ -1,4 +1,4 @@
-export const upper_first = (text: string): string => {
+export const capitalize_first = (text: string): string => {
   if (!text) return "";
 
   return text.charAt(0).toUpperCase() + text.slice(1);
