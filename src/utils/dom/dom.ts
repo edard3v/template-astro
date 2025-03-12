10cca5e3091ff9d7b83b6a6f72a64d393e85a73c
edard3v <edard3v@gmail.com>
2025-03-11 21:12:57 -0500

    refactor

--- src/utils/dom/dom.ts
+++ src/utils/dom/dom.ts
@@ -1,6 +1,5 @@
 export class Dom {
-  static select(id: string): HTMLElement | null {
-    const element = document.querySelector(`#${id}`);
-    return element as HTMLElement;
+  static select(id: string) {
+    return document.querySelector(`#${id}`);
   }
 }
