649cb64ffa4335cb89dc23d3cddb5680e023dc38
edard3v <edard3v@gmail.com>
2025-03-22 23:51:03 -0500

    refactor networks. ahora se utiliza Icon de astro-icons

--- src/components/navs/Network/networks.ts
+++ src/components/navs/Network/networks.ts
@@ -1,39 +1,26 @@
-import Discord from "./icons/Discord.astro";
-import Github from "./icons/Github.astro";
-import Instagram from "./icons/Instagram.astro";
-import Linkedin from "./icons/Linkedin.astro";
-import Youtube from "./icons/Youtube.astro";
-import X from "./icons/X.astro";
-
 export const NETWORKS = [
   {
-    display: "github",
     url: "https://github.com/edard3v",
-    Icon: Github,
+    name: "github",
   },
   {
-    display: "youtube",
     url: "https://www.youtube.com/@edard3v",
-    Icon: Youtube,
+    name: "youtube",
   },
   {
-    display: "linkedin",
     url: "https://www.linkedin.com/in/edard3v",
-    Icon: Linkedin,
+    name: "linkedin",
   },
   {
-    display: "discord",
     url: "https://discord.gg/HUfnRPjBHm",
-    Icon: Discord,
+    name: "discord",
   },
   {
-    display: "instagram",
     url: "https://www.instagram.com/edard3v/",
-    Icon: Instagram,
+    name: "instagram",
   },
   {
-    display: "X",
     url: "https://x.com/edard3v",
-    Icon: X,
+    name: "x",
   },
 ];
